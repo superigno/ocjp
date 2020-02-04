91127fa7cb5af78cc593cc177f35243a0e7f0c02
gino.q <gino.q@pure-commerce.com>
2020-02-04 15:48:42 +1100

    Edited note

--- src/main/java/com/superigno/initializationblock/Init.java
+++ src/main/java/com/superigno/initializationblock/Init.java
@@ -3,7 +3,10 @@ package com.superigno.initializationblock;
 /**
  * @note "...the non-static block is actually copied by the Java compiler into every constructor the class has (source). So it is still the constructor's job to initialize fields"
  *        https://stackoverflow.com/questions/2420389/static-initialization-blocks/2420404#comment22381342_2420466
- *        So parang no sense pala gamitin ang non-static initialization block -g1q *
+ *        So parang no sense pala gamitin ang non-static initialization block -g1q 
+ *        
+ *        Edit: May sense din, kung gsto mo tumawag ng something on both a no-arg constructor and with-arg constructor, lagay mo sa isang non-static initialization
+ *        block. Like below example.
  */
 public class Init {
 
